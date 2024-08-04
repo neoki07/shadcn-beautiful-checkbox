d6c28a6fd4daa5d186e9089bbc4b39b1c64a7444
neoki <ot07neoki@gmail.com>
2024-08-04 12:29:26 +0900

    feat: add copy button for code block

--- src/app/_components/code-block.tsx
+++ src/app/_components/code-block.tsx
@@ -1,3 +1,5 @@
+import { CopyButton } from "./copy-button"
+
 type CodeBlockProps = {
   src: string
 }
@@ -6,17 +8,20 @@ export function CodeBlock({ src }: CodeBlockProps) {
   const lines = src.trimEnd().split("\n")
 
   return (
-    <pre className="mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900">
-      <code className="relative grid rounded px-[0.3rem] py-[0.2rem] font-mono text-sm">
-        {lines.map((line, index) => (
-          <span
-            key={index}
-            className="inline-block min-h-4 w-full px-4 py-0.5 text-white"
-          >
-            {line}
-          </span>
-        ))}
-      </code>
-    </pre>
+    <div className="relative">
+      <pre className="mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900">
+        <code className="relative grid rounded font-mono text-sm">
+          {lines.map((line, index) => (
+            <span
+              key={index}
+              className="inline-block min-h-4 w-full px-4 py-0.5 text-white"
+            >
+              {line}
+            </span>
+          ))}
+        </code>
+      </pre>
+      <CopyButton value={src} className="absolute right-4 top-4" />
+    </div>
   )
 }
